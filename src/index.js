a4cba3483dd7dee9db134f3b415de6a294a2cf71
Guilherme Félix <guilherme.f.santos@outlook.com.br>
2025-06-25 21:10:01 -0300

    feat: implement playRaceEngine function to handle race logic and scoring

--- src/index.js
+++ src/index.js
@@ -32,3 +32,81 @@ async function logRollResult(characterName, block, diceResult, attribute) {
   );
 }
 
+async function playRaceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🏁 Round ${round}:\n`);
+
+    let block = await getRandomBlock();
+    console.log(`🚗 Block Type: ${block}`);
+
+    let diceResult1 = await rollDice();
+    let diceResult2 = await rollDice();
+
+    let totalTestSkill1 = 0;
+    let totalTestSkill2 = 0;
+
+    if (block === "STRAIGHT LINE") {
+      totalTestSkill1 = diceResult1 + character1.SPEED;
+      totalTestSkill2 = diceResult2 + character2.SPEED;
+
+      await logRollResult(player1.NAME, block, diceResult1, character1.SPEED);
+      await logRollResult(player2.NAME, block, diceResult2, character2.SPEED);
+    }
+
+    if (block === "TURN") {
+      totalTestSkill1 = diceResult1 + character1.MANEUVERABILITY;
+      totalTestSkill2 = diceResult2 + character2.MANEUVERABILITY;
+
+      await logRollResult(
+        player1.NAME,
+        block,
+        diceResult1,
+        character1.MANEUVERABILITY
+      );
+      await logRollResult(
+        player1.NAME,
+        block,
+        diceResult2,
+        character2.MANEUVERABILITY
+      );
+    }
+
+    if (block === "CLASH") {
+      let powerResult1 = diceResult1 + character1.POWER;
+      let powerResult2 = diceResult2 + character2.POWER;
+
+      console.log(`💥 ${character1.NAME} vs ${character2.NAME} Clash!`);
+      await logRollResult(player1.NAME, block, diceResult1, character1.POWER);
+      await logRollResult(player2.NAME, block, diceResult2, character2.POWER);
+
+      if (powerResult1 > powerResult2 && character2.SCORE > 0) {
+        console.log(
+          `🐢 ${character1.NAME} won the clash! ${character2.NAME} lost a point.`
+        );
+        character2.SCORE--;
+      }
+
+      if (powerResult2 > powerResult1 && character1.SCORE > 0) {
+        console.log(
+          `🐢 ${character2.NAME} won the clash! ${character1.NAME} lost a point.`
+        );
+        character1.SCORE--;
+      }
+
+      powerResult2 === powerResult1
+        ? console.log(`🤝 It's a tie! No score change.`)
+        : null;
+    }
+
+    if (totalTestSkill1 > totalTestSkill2) {
+      console.log(`🏆 ${character1.NAME} scored!`);
+      character1.SCORE++;
+    } else if (totalTestSkill2 > totalTestSkill1) {
+      console.log(`🏆 ${character2.NAME} scored!`);
+      character2.SCORE++;
+    }
+
+    console.log(`\n-------------------------------\n`);
+  }
+}
+
